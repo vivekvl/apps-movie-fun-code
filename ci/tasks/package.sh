e818ff9a4fc350af02c1debbe8cce23be37120d9
Dhiravidamani <423358@AMB00687.local>
2017-06-28 11:11:42 +0530

    concourse artifacts for movie fun

---
+++ ci/tasks/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+pushd movie-fun-source
+  echo "Packaging JAR"
+  ./mvnw clean package -DskipTests
+popd
+
+jar_count=`find movie-fun-source/target -type f -name *.war | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find movie-fun-source/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
+
+echo "Done packaging"
+exit 0
